e1125f8bb9ea05a9d74a0875c20759f42c2c0ef1
emillyme <emill.ymello212@gmail.com>
2025-03-18 00:22:03 -0300

    UPDATE

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // ou 'media' ou 'class'
+  theme: {
+    extend: {
+      colors: {
+        purple: "#9e2896",
+        green: "#308a85",
+        blue: "#007bc0",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
